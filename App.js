f1ecdb402b37a1ebb8bf5fba0efdb6ad31a5c9e2
Joni Hackzell <t1hajo05@students.oamk.fi>
2023-09-26 18:36:04 +0300

    koodit

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './HomeScreen';
+import SecondScreen from './SecondScreen';
 
 export default function App() {
+  const Stack= createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+          HeaderTitle: 'Home'
+        }}
+        />      
+        <Stack.Screen
+        name="Second"
+        component={SecondScreen}
+        options={{
+          title: 'Second',
+          HeaderTitle: 'Second'
+        }}
+        />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ HomeScreen.js
@@ -0,0 +1,39 @@
+import React, { useLayoutEffect, useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { AntDesign } from '@expo/vector-icons'
+
+export default function HomeScreen({navigation}) {
+    const [message, setMessage] = useState('Testing');
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerStyle: {
+                backgroundColor: 'grey'
+            },
+            headerRight: () => (
+                <AntDesign
+                    style={styles.navButton}
+                    name='arrowright'
+                    size={24}
+                    onPress={() => navigation.navigate('Second',{message: message})}
+                    />
+            )
+        })
+    }, [message])
+    return (
+        <View style={styles.container}>
+            <Text>Home Screen{message}</Text>
+            <TextInput onChangeText={text => setMessage(text)} value={message} placeholder="Type message here"/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+    },
+    navButton: {
+        marginRight: 5,
+        padding: 4,
+    }
+});
